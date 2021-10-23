67ee1ce1d45d64d885a484dd509822f4c2fbe492
dhanuj001 <61634217+dhanuj001@users.noreply.github.com>
2021-10-23 18:43:52 +0530

    added a search bar in explore page and explore.css

--- src/components/explore.js
+++ src/components/explore.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./navbar";
+import { Search as _Search } from "react-feather";
+import "../css/explore.css";
 
 export default function explore() {
-    return (
-        <div>
-            explore
+  return (
+    <div>
+      <Navbar />
+      <div className="search-box-div">
+        <div className="search-box">
+          <_Search size={30} />
+          <input type="text" placeholder="Find faculty.." />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
