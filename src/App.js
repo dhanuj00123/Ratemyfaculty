7a3863ab6ffb94e2a1174f298e35f768eae1c1be
dhanuj001 <61634217+dhanuj001@users.noreply.github.com>
2021-10-23 14:04:02 +0530

    starting changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
+import "./App.css";
+import { BrowsweRouter as Router, Switch, Route } from "react-router-dom";
+import home from "./components/home";
+import explore from "./components/explore";
+import profile from "./components/profile";
+import navbar from "./components/navbar";
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Route>
+      <navbar />
+      <Switch>
+        <Route path="/" exact component={home} />
+        <Route path="/explore" component={explore} />
+        <Route path="/profile" component={profile} />
+      </Switch>
+    </Route>
   );
 }
 
