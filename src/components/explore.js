00ce30016f7cc7d5756654eab8d39750633efd72
dhanuj001 <61634217+dhanuj001@users.noreply.github.com>
2021-10-25 13:43:38 +0530

    department wise  only 5 faculty render in explore.js

--- src/components/explore.js
+++ src/components/explore.js
@@ -28,20 +28,59 @@ class explore extends React.Component {
         <div className="search-box-div">
           <Searchbar />
         </div>
-        <div className="cards">
-          {this.state.results
-            .filter((faculty) => faculty.dept_id === "CSE")
-            .map((faculty) => (
-              <Cards
-                key={faculty.faculty_id}
-                name={faculty.faculty_name}
-                img={faculty.faculty_img_url}
-                rating={faculty.faculty_rating}
-                position={faculty.faculty_position}
-              />
-            ))}
+        <div className="main-div">
+          <p className="title">Computer Science</p>
+          <div className="cse-div-child">
+            {this.state.results
+              .filter((faculty) => faculty.dept_id === "CSE")
+              .slice(0, 5)
+              .map((faculty) => (
+                <Cards
+                  key={faculty.faculty_id}
+                  name={faculty.faculty_name}
+                  img={faculty.faculty_img_url}
+                  rating={faculty.faculty_rating}
+                  position={faculty.faculty_position}
+                />
+              ))}
+          </div>
+        </div>
+        <div className="main-div">
+          <p className="title">Physics</p>
+          <div className="cse-div-child">
+            {this.state.results
+              .filter((faculty) => faculty.dept_id === "PHY")
+              .slice(0, 5)
+              .map((faculty) => (
+                <Cards
+                  key={faculty.faculty_id}
+                  name={faculty.faculty_name}
+                  img={faculty.faculty_img_url}
+                  rating={faculty.faculty_rating}
+                  position={faculty.faculty_position}
+                />
+              ))}
+          </div>
+        </div>
+        <div className="main-div">
+          <p className="title">Mechanical Engineering</p>
+          <div className="cse-div-child">
+            {this.state.results
+              .filter((faculty) => faculty.dept_id === "MECH")
+              .slice(0, 5)
+              .map((faculty) => (
+                <Cards
+                  key={faculty.faculty_id}
+                  name={faculty.faculty_name}
+                  img={faculty.faculty_img_url}
+                  rating={faculty.faculty_rating}
+                  position={faculty.faculty_position}
+                />
+              ))}
+          </div>
         </div>
       </div>
+      // </div>
     );
   }
 }
